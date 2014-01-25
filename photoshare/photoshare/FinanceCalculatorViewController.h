033822d1fc038eaf44ad1178baa784ce24a843be
ignis3 <ignis3@ignis3s-Mac-Pro.local>
2014-01-25 16:04:30 +0530

    Finance Calculator, Pastpay, custom cell created

---
+++ photoshare/photoshare/FinanceCalculatorViewController.h
@@ -0,0 +1,31 @@
+//
+//  FinanceCalculatorViewController.h
+//  photoshare
+//
+//  Created by ignis3 on 25/01/14.
+//  Copyright (c) 2014 ignis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FinanceCalculatorViewController : UIViewController
+{
+    IBOutlet UISegmentedControl *segmentControl;
+    IBOutlet UILabel *amountCalculated;
+    IBOutlet UILabel *currencySetter;
+    IBOutlet UITextField *border1; //for 1st gem
+    IBOutlet UITextField *border2; //for 2nd gem
+    IBOutlet UITextField *border3; //for 3rd gem
+    IBOutlet UILabel *firstGem; 
+    IBOutlet UILabel *secondGem;
+    IBOutlet UILabel *thirdGem;
+    IBOutlet UIButton *firstGemUP_btn;
+    IBOutlet UIButton *firstGemDown_btn;
+    IBOutlet UIButton *secondGemUP_btn;
+    IBOutlet UIButton *secondGemDown_btn;
+    IBOutlet UIButton *thirdGemUP_btn;
+    IBOutlet UIButton *thirdGemDown_btn;
+}
+
+-(IBAction)segmentedControlIndexChanged;
+@end
